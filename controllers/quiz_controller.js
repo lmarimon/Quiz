33963524d1bd89fc46cbf1323ddb694655a813b1
Job3 <job3.14.2005@gmail.com>
2015-08-03 18:08:41 +0200

    Crear preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -6,7 +6,7 @@ exports.load = function (req, res, next, quizId) {
 		if (quiz){
 			req.quiz = quiz;
 			next();
-		} else { next(new Error('No existe quizId=' + quizId);) }
+		} else { next(new Error('No existe quizId=' + quizId)); }
 	}).catch(function(error) { next(error); });
 };
 
@@ -29,4 +29,22 @@ exports.answer = function (req, res) {
 		resultado = 'Correcto';
 	}
 	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+};
+
+// GET /quizes/new
+exports.new = function (req, res) {
+	var quiz = models.Quiz.build( // crea objeto quiz
+		{pregunta: "Pregunta", respuesta: "Respuesta"}
+	);
+	res.render('quizes/new', {quiz: quiz});
+};
+
+// POST /quizes/create
+exports.create = function (req, res) {
+	var quiz = models.Quiz.build( req.body.quiz );
+
+	// guarda en DB los campos pregunta y respuesta de quiz
+	quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+		res.redirect('/quizes');
+	}) // Redirección HTTP (URL relativo) lista de preguntas
 };

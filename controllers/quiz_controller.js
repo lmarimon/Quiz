d16aa6148af293de72a4c77e329249c454b8f3e0
Job3 <job3.14.2005@gmail.com>
2015-08-06 21:53:57 +0200

    Buscador de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -12,7 +12,10 @@ exports.load = function (req, res, next, quizId) {
 
 // GET /quizes
 exports.index = function (req, res) {
-	models.Quiz.findAll().then(function(quizes) {
+	var search = req.query.search || '%';
+	search = '%' + search.replace(' ', '%') + '%';
+	
+	models.Quiz.findAll({where: ['pregunta like ?', search]}).then(function(quizes) {
 		res.render('quizes/index', {quizes: quizes, errors: []});
 	}).catch(function(error) { next(error); });
 };
